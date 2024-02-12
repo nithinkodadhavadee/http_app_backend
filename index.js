94c288ae2ece174f0b3115a38535ad76a1551672
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2024-02-12 19:47:50 +0530

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3001;
 
 let ledStatus = '0000';
 let microIp = 'http://192.168.1.13';
